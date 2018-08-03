c475240ed0fd0b40167aa9ab2ad36a738323402d
Simon Peter Ssekamatte <spssekamatte@gmail.com>
2018-08-03 09:34:40 +0300

    feat(mock_db): Create a mock database
    - Populate mock database with people and acronyms

---
+++ firebase/functions/mock_db/acronyms.js
@@ -0,0 +1,30 @@
+static const acronyms = {
+  epic: {
+    acronym: 'EPIC',
+    description: 'Excellence Passion Integrity Collaboration',
+  },
+  erd: {
+    acronym: 'ERD',
+    description: 'Entity Relationship Diagram',
+  },
+  yoyo: {
+    acronym: 'YOYO',
+    description: 'You Own Your Own',
+  },
+  wip: {
+    acronym: 'WIP',
+    description: 'Work In Progress',
+  },
+  mvp: {
+    acronym: 'MVP',
+    description: 'Minimum Viable Product',
+  },
+  sdk: {
+    acronym: 'SDK',
+    description: 'Software Development Kit',
+  },
+  ide: {
+    acronym: 'IDE',
+    description: 'Integrated Development Environment',
+  },
+}
---
+++ firebase/functions/mock_db/people.js
@@ -0,0 +1,12 @@
+static const people = {
+  jeremy_johnshon: {
+    department: 'Management',
+    name: 'Jeremy Johnson',
+    role: 'CEO',
+  },
+  simon_ssekamatte: {
+    department: 'Technology',
+    name: 'Simon Ssekamatte',
+    role: 'Long Term Engineer on the Pulse Team'
+  }
+}
